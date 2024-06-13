35c4de7b05bb7ac32f6b6a671a5768b69132a8f5
SebastianRiveros <sriveros@unsa.edu.pe>
2024-06-12 20:30:50 -0500

    creacion de una nueva vista

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def myHomeView(*args, **kwargs):
 	return HttpResponse('<h1>Hola mundo desde Django</h1>')
+
+def anotherView(request):
+	return HttpResponse('<h1>Solo otra pagina</h1>')
--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
+from inicio.views import anotherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', myHomeView, name='Pagina de Inicio'),
+    path('another/', anotherView),
 ]
