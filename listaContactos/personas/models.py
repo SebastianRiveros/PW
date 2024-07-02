fb97e36973a54274401e881aacf059391c9e966d
SebastianRiveros <sriveros@unsa.edu.pe>
2024-07-01 22:43:13 -0500

    Examinando los objetos del modelo Persona desde la shell

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Persona(models.Model):
 	nombres = models.CharField(max_length = 100)
 	apellidos = models.CharField(max_length = 100)
-	edad = models.IntegerField()	
+	edad = models.IntegerField(blank = True)	
 	donador = models.BooleanField()
