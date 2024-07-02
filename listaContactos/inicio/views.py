66913417f8d8bc950c3b80cfbc5d4601e4b7bee0
SebastianRiveros <sriveros@unsa.edu.pe>
2024-07-01 22:15:01 -0500

    Uso de variables del contexto en un template

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def myHomeView(request, *args, **kwargs):
+	myContext = {
+		'myText' : 'Esto es sobre nosotros',
+		'myNumber' : '123',
+	}
 	print(args, kwargs)
 	print(request.user)
-	return render(request,"home.html",{})
+	return render(request,"home.html",myContext)
 
 def anotherView(request):
 	return HttpResponse('<h1>Solo otra pagina</h1>')
